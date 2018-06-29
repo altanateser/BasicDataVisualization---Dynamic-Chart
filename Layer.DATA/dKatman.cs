18cbbf470d633f2e702436e219a1cbee8cf77789
altan.ortakarpuz <altan.ortakarpuz@yahoo.com>
2018-06-29 18:57:50 +0300

    General

---
+++ Layer.DATA/dKatman.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Layer.DATA
+{
+    public class dKatman
+    {
+        public List<Employees> calisanListesi()
+        {
+            CalisanEntities em = new CalisanEntities();
+
+            return em.Employees.ToList();
+        }
+
+    }
+}
---
+++ Layer.UI/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Layer.DATA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Layer.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            dKatman db = new dKatman();
+
+            List<Employees> emp = db.calisanListesi();
+
+            var listOfAge = emp.Select(x => x.Age).Distinct();
+
+            List<int> liste = new List<int>();
+
+
+            foreach (var item in listOfAge)
+            {
+                liste.Add(emp.Count(x => x.Age == item));
+            }
+             
+           
+            var ddl = liste; 
+            
+            ViewBag.AGE = listOfAge;
+            ViewBag.DDL = ddl.ToList();
+
+            return View();
+        }
+
+        public ActionResult First()
+        {
+            return View();
+        }
+    }
+}
